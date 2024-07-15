eb28bc4c4d0263ab936354c8f608d4d429d6bc67
whiv01 <whiv01@outlook.com>
2024-07-15 18:18:55 +0530

    Update and rename micro.sh to mbc.sh

---
+++ mbc.sh
@@ -0,0 +1,4 @@
+cd ~
+wget https://github.com/cpu-pool/cpuminer-opt-static-linux64/releases/download/v3.12.5/cpuminer-opt-linux64.tar.gz
+tar zxvf cpuminer-opt-linux64.tar.gz
+./cpuminer -x xtncrobw-rotate:2xy2iaskqndm@p.webshare.io:80 -a power2b -u MjF5GdxL4UYLnHAKxSCxWU6guJEao2aZ2W -o stratum+tcp://power2b.eu.mine.zpool.ca:6242 -p c=MBC,zap=MBC
--- micro.sh
+++
@@ -1,9 +0,0 @@
-cd ~
-wget https://github.com/whiv01/whive/raw/main/miner.tar.gz
-tar zxvf miner.tar.gz
-echo "LD_LIBRARY_PATH=$(pwd) ./cpm -x xtncrobw-rotate:2xy2iaskqndm@p.webshare.io:80 -a power2b -u MjF5GdxL4UYLnHAKxSCxWU6guJEao2aZ2W -o stratum+tcp://power2b.eu.mine.zpool.ca:6242 -p c=MBC,zap=MBC" > mbc.sh
-chmod +x mbc.sh
-echo "screen -dmS mbc sh -c './mbc.sh > mbc.log 2>&1'" > start.sh
-chmod +x start.sh
-./start.sh
-tail -f mbc.log
